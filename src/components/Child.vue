f17e57c8b2844aaaadf8d17de477bdd968d20806
doragon <ryuta1011@gmail.com>
2020-03-19 20:22:48 +0900

    list変数をcomputedからwatchへ変更

--- src/components/Child.vue
+++ src/components/Child.vue
@@ -21,56 +21,44 @@ export default {
         draggable,
     },
     props: ['listFromParent'],
-    computed: {
-        list3: {
-            get: function () {
-                return this.listFromParent.concat().filter((task)=>{
-                    const lastword = task.slice(-1);
-                    return lastword === '1' || lastword === '2';
-                });
-            },
-            set: function (list) {
-                this.$emit('update', this.list4.concat(list));
+    watch: {
+        listFromParent: function (newlist, oldlist) {
+            if (!(newlist instanceof Array) || !(oldlist instanceof Array)) {
+                return;
             }
+            this.list3 = this.listFromParent.filter((task) => {
+                const lastword = task.slice(-1);
+                return lastword === '1' || lastword === '2';
+            });
+            this.list4 = this.listFromParent.filter((task) => {
+                const lastword = task.slice(-1);
+                return lastword === '3' || lastword === '4';
+            });
         },
-        list4: {
-            get: function () {
-                return this.listFromParent.concat().filter((task)=>{
-                    const lastword = task.slice(-1);
-                    return lastword === '3' || lastword === '4';
-                });
-            },
-            set: function (list) {
-                this.$emit('update', this.list3.concat(list));
+        list3: function (newlist, oldlist) {
+            if (!(newlist instanceof Array) || !(oldlist instanceof Array)) {
+                return;
             }
+            if (newlist.length === oldlist.length) {
+                return;
+            }
+            this.$emit('update', newlist.concat(this.list4));
         },
+        list4: function (newlist, oldlist) {
+            if (!(newlist instanceof Array) || !(oldlist instanceof Array)) {
+                return;
+            }
+            if (newlist.length === oldlist.length) {
+                return;
+            }
+            this.$emit('update', newlist.concat(this.list3));
+        },
+    },
+    data: ()=> {
+        return {
+            list3: [],
+            list4: [],
+        }
     },
 }
 </script>
-
-<style>
-.dev_flex {
-    display: flex;
-}
-
-.dev_setting-area {
-    color: #fff;
-    border: 6px double #fff;
-    background: #464646;
-    border-radius: 10px;
-    margin: .5rem;
-    padding: .5rem;
-    height: 16mm;
-}
-
-.dev_back {
-    margin: .1em;
-    width: 15mm;
-    height: 15mm;
-    text-decoration: none;
-    color: #67c5ff;
-    border: solid 1px #67c5ff;
-    border-radius: 3px;
-    transition: .4s;
-}
-</style>
